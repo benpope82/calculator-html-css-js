4470e34bfe9e32dde517cd5127199f7fc44145e1
Roy Scheffers <r.scheffers@gmail.com>
2016-06-19 23:00:09 -0400

    Add onclick event Research var / element assignment

--- main_script.js
+++ main_script.js
@@ -2,3 +2,11 @@
 $( document ).ready(function() {
 
 });
+
+// next step assign an element to a var, so it works easier in the code 
+var display = $('.display').html();
+	console.log(display);
+
+$('.button').on("click", function(e) {
+	console.log(e.currentTarget.textContent);
+});
