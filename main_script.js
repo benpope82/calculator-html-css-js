ce475d66c79c2fd9a72dc174eb3819283c31dbdb
Roy Scheffers <r.scheffers@gmail.com>
2016-06-21 13:39:55 -0400

    remove sub-display, html, css and js to keep the project within scope

--- main_script.js
+++ main_script.js
@@ -9,16 +9,16 @@ $( document ).ready(function() {
 
 // next step assign an element to a var, so it works easier in the code 
 // var display = $('.display').html();
-	var display = document.getElementById('main-display');
+	var display = document.getElementById('display');
 	console.log(display);
 $('.button').on("click", function(e) {
 	var value = e.currentTarget.textContent;
 	switch (value) {
-		case 'AC': document.getElementById('main-display').textContent = '0';
+		case 'AC': document.getElementById('display').textContent = '0';
 		break;
 		case '1' : alert();
 		break;
-		default : document.getElementById('main-display').textContent += e.currentTarget.textContent;
+		default : document.getElementById('display').textContent += e.currentTarget.textContent;
 	}
 	console.log(e.currentTarget.textContent);
 	
